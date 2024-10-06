db8f70ad15015931a4c71994e94bf44cfb1c76ed
IT21314742 <it21314742@my.sliit.lk>
2024-10-06 18:36:26 +0530

    Update
    Update

--- formelements2.py
+++ formelements2.py
@@ -15,7 +15,7 @@ my_style.configure('success.Outline.TButton', font=("Helvetica", 18))
 
 my_button = tb.Button(text="Hello World!",
                       bootstyle="success",
-                      style="success.Op.TButton")
+                      style="success.p.TButton")
 
 my_button.pack(pady=40)
 
